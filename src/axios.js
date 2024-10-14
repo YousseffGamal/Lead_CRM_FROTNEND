742b9232c6a99a2ec7951f694406bfee10bdbc82
Hanin Khairy <hanindahb007@gmail.com>
2024-10-14 12:42:07 +0300

    ax

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: "http://localhost:4000", // Replace with your API base URL
-  timeout: 10000, // Optional: Set a timeout for requests
+  timeout: 20000, // Optional: Set a timeout for requests
 });
 
 // Add a request interceptor
